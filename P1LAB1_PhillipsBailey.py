1027fbcdbf9f4aa67cbf8331d60500125f027cf7
Zeno333 <baileyphillips06@gmail.com>
2026-02-21 13:34:14 -0500

    Add files via upload

---
+++ P1LAB1_PhillipsBailey.py
@@ -0,0 +1,9 @@
+# Bailey Phillips
+# 02/21/2026
+# P1LAB1
+# This program asks the user for a first and last name and prints a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
